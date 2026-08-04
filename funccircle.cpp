8f0f78216a93da30f5f7e6647c772cdc9a8d21fa
shwetakulkarni1106-hub <shwetakulkarni1106@gmail.com>
2026-08-04 08:38:05 +0530

    Add files via upload

---
+++ funccircle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+void circle(int radius)
+{
+  cout<<"The area of circle is: "<<3.14*radius*radius<<endl;
+  cout<<"The circumference of circle is: "<<2*3.14*radius<<endl;
+}
+int main()
+{
+  circle(2);
+}    
